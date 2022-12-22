dcd71e6a08e02482ae97c289f8748f839de51170
Radek <radek.palisa@gmail.com>
2022-12-22 10:57:23 +0100

    initial setup

---
+++ lua/radekpalisa/core/options.lua
@@ -0,0 +1,42 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tab & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+-- Start scrolling when you're 15 away from bottom (and side)
+vim.opt.scrolloff = 15
+vim.opt.sidescrolloff = 35
---
+++ lua/radekpalisa/plugins/lualine.lua
@@ -0,0 +1,13 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+	return
+end
+
+lualine.setup({
+	sections = {
+		lualine_c = {
+			{ "filename", path = 1 },
+		},
+	},
+})
